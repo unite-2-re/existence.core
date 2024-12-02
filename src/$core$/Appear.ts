cd4079fef88c2f178c1d1785fab7773f2bac12a5
unit-404 <conway24b@gmail.com>
2024-12-02 14:36:54 +0700

    before events

--- src/$core$/Appear.ts
+++ src/$core$/Appear.ts
@@ -54,59 +54,66 @@ export const observeAttributeBySelector = (element, selector, attribute, cb) => 
 //
 const computed = Symbol("@computed");
 const animateHide = async (target)=>{
-    if (!matchMedia("(prefers-reduced-motion: reduce)").matches && !target.classList.contains("u2-while-animation") && !target.hasAttribute("data-instant")) {
-        target.classList.add("u2-while-animation");
-    }
-
     //
-    if (target.classList.contains("u2-while-animation")) {
-        //target[computed] = getComputedStyle(target, "");
-        await target.animate([
-            {
-                easing: "linear",
-                offset: 0,
-
-                //
-                display: target[computed]?.display || "revert-layer",
-                "--opacity": target[computed]?.getPropertyValue("--opacity") || "revert-layer",
-                "--scale": target[computed]?.getPropertyValue("--scale") || "revert-layer",
-                pointerEvents: "none"
-            },
-            {
-                easing: "linear",
-                offset: 0.99,
-
-                //
-                display: target[computed]?.display || "revert-layer",
-                "--opacity": 0,
-                "--scale": 0.8,
-                pointerEvents: "none"
-            },
-            {
-                easing: "linear",
-                offset: 1,
-
-                //
-                display: "none",
-                "--opacity": 0,
-                "--scale": 0.8,
-                pointerEvents: "none"
-            }
-        ],  {
-            fill: "none",
-            duration: 100,
-            easing: "linear",
-            delay: matchMedia("(hover: none)").matches ? (parseFloat(target.getAttribute("data-delay-hide") || "0") || 0) : 0
-            //rangeStart: "cover 0%",
-            //rangeEnd: "cover 100%",
-        }).finished;
+    if (target?.dispatchEvent?.(new CustomEvent("u2-before-hide", {
+        detail: {},
+        bubbles: true,
+        cancelable: true
+    }))) {
+        if (!matchMedia("(prefers-reduced-motion: reduce)").matches && !target.classList.contains("u2-while-animation") && !target.hasAttribute("data-instant")) {
+            target.classList.add("u2-while-animation");
+        }
 
         //
-        target.classList.remove("u2-while-animation");
+        if (target.classList.contains("u2-while-animation")) {
+            //target[computed] = getComputedStyle(target, "");
+            await target.animate([
+                {
+                    easing: "linear",
+                    offset: 0,
+
+                    //
+                    display: target[computed]?.display || "revert-layer",
+                    "--opacity": target[computed]?.getPropertyValue("--opacity") || "revert-layer",
+                    "--scale": target[computed]?.getPropertyValue("--scale") || "revert-layer",
+                    pointerEvents: "none"
+                },
+                {
+                    easing: "linear",
+                    offset: 0.99,
+
+                    //
+                    display: target[computed]?.display || "revert-layer",
+                    "--opacity": 0,
+                    "--scale": 0.8,
+                    pointerEvents: "none"
+                },
+                {
+                    easing: "linear",
+                    offset: 1,
+
+                    //
+                    display: "none",
+                    "--opacity": 0,
+                    "--scale": 0.8,
+                    pointerEvents: "none"
+                }
+            ],  {
+                fill: "none",
+                duration: 100,
+                easing: "linear",
+                delay: matchMedia("(hover: none)").matches ? (parseFloat(target.getAttribute("data-delay-hide") || "0") || 0) : 0
+                //rangeStart: "cover 0%",
+                //rangeEnd: "cover 100%",
+            }).finished;
+
+            //
+            target.classList.remove("u2-while-animation");
+        }
     }
 
     //
-    target.dispatchEvent(new CustomEvent("u2-hidden", {
+    target?.dispatchEvent?.(new CustomEvent("u2-hidden", {
         detail: {},
         bubbles: true,
         cancelable: true
@@ -115,58 +122,66 @@ const animateHide = async (target)=>{
 
 //
 const animateShow = async (target)=>{
-    if (!matchMedia("(prefers-reduced-motion: reduce)").matches && !target.classList.contains("u2-while-animation") && !target.hasAttribute("data-instant")) {
-        target.classList.add("u2-while-animation");
-    }
-
     //
-    if (target.classList.contains("u2-while-animation")) {
-        await target.animate([
-            {
-                easing: "linear",
-                offset: 0,
-
-                //
-                display: "none",
-                "--opacity": 0,
-                "--scale": 0.8,
-                pointerEvents: "none"
-            },
-            {
-                easing: "linear",
-                offset: 0.01,
-
-                //
-                display: target[computed]?.display || "revert-layer",
-                "--opacity": 0,
-                "--scale": 0.8,
-                pointerEvents: "none"
-            },
-            {
-                easing: "linear",
-                offset: 1,
-
-                //
-                display: target[computed]?.display || "revert-layer",
-                "--opacity": target[computed]?.getPropertyValue("--opacity") || "revert-layer",
-                "--scale": target[computed]?.getPropertyValue("--scale") || "revert-layer",
-                pointerEvents: target[computed]?.pointerEvents || "revert-layer"
-            }
-        ], {
-            fill: "none",
-            duration: 100,
-            easing: "linear",
-            delay: 0
-            //rangeStart: "cover 0%",
-            //rangeEnd: "cover 100%",
-        }).finished;
+    if (target?.dispatchEvent?.(new CustomEvent("u2-before-show", {
+        detail: {},
+        bubbles: true,
+        cancelable: true
+    }))) {
+        //
+        if (!matchMedia("(prefers-reduced-motion: reduce)").matches && !target.classList.contains("u2-while-animation") && !target.hasAttribute("data-instant")) {
+            target.classList.add("u2-while-animation");
+        }
 
         //
-        target.classList.remove("u2-while-animation");
+        if (target.classList.contains("u2-while-animation")) {
+            await target.animate([
+                {
+                    easing: "linear",
+                    offset: 0,
+
+                    //
+                    display: "none",
+                    "--opacity": 0,
+                    "--scale": 0.8,
+                    pointerEvents: "none"
+                },
+                {
+                    easing: "linear",
+                    offset: 0.01,
+
+                    //
+                    display: target[computed]?.display || "revert-layer",
+                    "--opacity": 0,
+                    "--scale": 0.8,
+                    pointerEvents: "none"
+                },
+                {
+                    easing: "linear",
+                    offset: 1,
+
+                    //
+                    display: target[computed]?.display || "revert-layer",
+                    "--opacity": target[computed]?.getPropertyValue("--opacity") || "revert-layer",
+                    "--scale": target[computed]?.getPropertyValue("--scale") || "revert-layer",
+                    pointerEvents: target[computed]?.pointerEvents || "revert-layer"
+                }
+            ], {
+                fill: "none",
+                duration: 100,
+                easing: "linear",
+                delay: 0
+                //rangeStart: "cover 0%",
+                //rangeEnd: "cover 100%",
+            }).finished;
+
+            //
+            target.classList.remove("u2-while-animation");
+        }
     }
 
     //
-    target.dispatchEvent(new CustomEvent("u2-appear", {
+    target?.dispatchEvent?.(new CustomEvent("u2-appear", {
         detail: {},
         bubbles: true,
         cancelable: true
@@ -252,4 +267,5 @@ const initialize = ()=>{
     });
 }
 
+//
 export default initialize;
